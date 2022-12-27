f1439c50ab88a509953f0956731853fd7748c3a6
thanabordeee <jejekup00123@hotmail.co.th>
2022-12-27 16:43:55 +0700

    complete

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -3,10 +3,21 @@
 using namespace std;
 
 int main(){	
-	cout << "Enter initial loan: ";
-	cout << "Enter interest rate per year (%): ";
-	cout << "Enter amount you can pay per year: ";
+	double loan,inter,payback;
+	int year=1;
+	double PrevBalance,total,Newbalance,interest=0;
+	
 
+	cout << "Enter initial loan: ";
+	cin >> loan;
+	cout << "Enter interest rate per year (%): ";
+	cin >> inter;
+	cout << "Enter amount you can pay per year: ";
+	cin >> payback;
+   
+
+
+ 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
 	//Try to change from 'left' to 'right' and see the effect
@@ -20,14 +31,35 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
+
+    Newbalance = 1;
+	while(!(Newbalance == 0)) 
+	{
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
+	cout << setw(13) << left << year;
+	PrevBalance = loan; 
+	cout << setw(13) << left << PrevBalance;
+	interest = loan*(inter/100);
+	cout << setw(13) << left << interest;
+	total = PrevBalance + interest;
+	cout << setw(13) << left << total;
+	if (total < payback)
+	{
+		payback = total;
+	}
+	cout << setw(13) << left << payback;
+    Newbalance = total-payback;
+	cout << setw(13) << left << Newbalance;
 	cout << "\n";	
+	year++;
+    loan = Newbalance;
 	
+	}
+	
+	
+
+	
+	
+
 	return 0;
 }
--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,7 +1,23 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
+void printO(int N, int M)
+{
+if (N<=0 || M<=0)
+{
+	cout << "Invalid input";
+}
+else{
+for (int i = 0; i < N; i++)
+{
+	for (int j = 0; j < M; j++)
+	{
+		cout << "O";;
+	}
+	cout << '\n';
+}
+}
+}
 
 int main(){
 	
